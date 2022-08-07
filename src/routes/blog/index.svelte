ff50f307ae0460e79c49d695c0fc30fbb15241cf
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-08-07 09:10:39 -0400

    Add search param and query param functionality

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -5,7 +5,7 @@
                 <h1>{ limit ? 'RECENT' : 'ALL' } POSTS {#if limit}<span ng-show="limit" class="show-all" on:click={() => toggleLimit()}>Show all</span>{/if}</h1>
             </td>
             <td class="search">
-                <input type="text" ng-model="searchValue" placeholder="SEARCH POSTS" />
+                <input type="text" bind:value={searchValue} on:input={() => updateSearch()} placeholder="SEARCH POSTS" />
             </td>
         </tr>
     </table>
@@ -25,24 +25,55 @@
 </svelte:head>
 
 <script>
+    import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
     import { blogPages, currentPage } from './blog';
 
     currentPage.set(null);
 
-    let limit = false;
+    let limit;
     let pages = [];
+    let searchValue;
+
+    page.subscribe(() => {
+        searchValue = typeof searchValue === 'undefined' ? $page.url.searchParams.get("search") : searchValue;
+        limit = typeof limit === 'undefined' ? !$page.url.searchParams.get("showAll") : limit;
+    });
 
     const toggleLimit = () => {
         limit = !limit;
 
+        updateQueryParam("showAll", !limit);
+
+        updateResults();
+    };
+
+    const updateSearch = () => {
+        updateQueryParam("search", searchValue);
+
+        updateResults();
+    };
+
+    const updateQueryParam = (name, value) => {
+        if (!value) {
+            $page.url.searchParams.delete(name);
+        } else {
+            $page.url.searchParams.set(name, value);
+        }
+
+        goto(`?${$page.url.searchParams.toString()}`, {replaceState: false, noscroll: true, keepfocus: true});
+    }
+
+    const updateResults = () => {
         pages = [...blogPages]
             .filter(p => p.visible !== false)
+            .filter(p => !searchValue || p.header.toLocaleLowerCase().indexOf(searchValue.toLocaleLowerCase()) !== -1)
             .sort((a, b) => b.dateObj.getTime() - a.dateObj.getTime());
 
         if (limit) {
             pages = pages.slice(0, 10);
         }
-    };
+    }
 
-    toggleLimit();
+    updateResults();
 </script>
