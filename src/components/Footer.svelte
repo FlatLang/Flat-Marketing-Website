08e3b773283888bc21dd209e3fd599586c49ae3e
Braden Steffaniak <braden@novex.tech>
2022-08-08 19:03:41 -0400

    Do not show social icons in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,6 +3,7 @@
         <tr>
             <td><a class="bold" href="/docs">DOCUMENTATION</a></td>
             <td><a class="bold" href="mailto:info@flat-lang.org">INFO@FLAT-LANG.ORG</a></td>
+            {#if false}
             <td class="social">
                 <a href="https://www.facebook.com/NovexTech">
                     <img src="/images/facebook.svg" />
@@ -11,6 +12,7 @@
                     <img src="/images/twitter.svg" />
                 </a>
             </td>
+            {/if}
         </tr>
     </table>
 </slide>
