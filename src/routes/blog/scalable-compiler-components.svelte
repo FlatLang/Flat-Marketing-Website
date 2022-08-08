5ca08b0fea37fbb29ffdf2486aaf3d40bdfc7245
Braden Steffaniak <braden@novex.tech>
2022-08-08 16:55:26 -0400

    Fix broken link

--- src/routes/blog/scalable-compiler-components.svelte
+++ src/routes/blog/scalable-compiler-components.svelte
@@ -144,7 +144,7 @@
     <h1>HOW COMPILER MODULES ARE LOADED</h1>
     <p><i><b>Disclaimer</b>: The following code is just demo code to give a general idea of what the implementation looks like. There will be individual blog posts on each of the specific component types in the future where the implementation details and specifics are shared.</i></p>
     <p>
-        The compiler looks for classes that extend or implement specific classes or interfaces to determine what components to run for each stage of compilation. After loading the Library (explained in the <a href="blog.runtime-module-loading">previous blog post</a>), you can search for classes that extended or implement a specific class or interface with the Class.getClassesOfType function<footnote id="class-filtering"></footnote>.
+        The compiler looks for classes that extend or implement specific classes or interfaces to determine what components to run for each stage of compilation. After loading the Library (explained in the <a href="/blog/runtime-module-loading">previous blog post</a>), you can search for classes that extended or implement a specific class or interface with the Class.getClassesOfType function<footnote id="class-filtering"></footnote>.
     </p>
     <div id="parsers">
         <h3>PARSER COMPONENTS</h3>
