1af3580e15c2cc39b54c4e4115d9460e6d8bc8ad
Braden Steffaniak <braden@novex.tech>
2022-08-08 18:43:31 -0400

    Handle more hash flash edge cases

--- src/flash.ts
+++ src/flash.ts
@@ -1,7 +1,11 @@
+let lastFlashed;
+
 export function checkHash() {
   const hash = window.location.hash?.substring(1);
 
   if (hash) {
+    lastFlashed?.classList.remove("flash");
+
     const element = document.getElementById(hash);
 
     element?.classList.remove("flash");
@@ -10,15 +14,23 @@ export function checkHash() {
       element?.classList.add("flash");
       element?.scrollIntoView();
     }, 0);
+
+    lastFlashed = element;
   }
 }
 
 if (typeof document !== 'undefined') {
+  window.addEventListener("hashchange", checkHash, false);
+
   document.addEventListener("click", ({target}) => {
     const href = target?.getAttribute("href");
 
     if (typeof href === 'string' && href[0] === '#') {
-      checkHash();
+      const hash = window.location.hash;
+
+      if (hash === href) {
+        checkHash();
+      }
     }
   });
 }
