e89cae1383f4bc760c433ea3f4e85dceba0abd35
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-08-12 18:08:48 -0400

    Clean up download page content

--- src/routes/download/index.svelte
+++ src/routes/download/index.svelte
@@ -6,43 +6,17 @@
 </svelte:head>
 
 <template lang="flat-html">
-<div class="white-background">
+<div class="white-background download">
     <div class="page-container">
         <Header></Header>
-        <div class="content-container">
+        <div id="content-container">
             <div use:slide id="downloads" class="slide">
                 <div><h1 class="primary">DOWNLOAD</h1></div>
                 <hr>
                 <h4 class="gray">We think you are running { osHeader }</h4>
-                <p ng-if="!currentOs">{ os } is not currently supported by Flat.</p>
-                <div ng-repeat="type in downloadTypes" id="{ type.id }" ng-if="type.version">
-                    <h2>{ (build || type.name).toUpperCase() } v{ buildVersion || type.version } <span class="show-all gray" ng-click="toggleShowAll(type)">show { type.showAll ? versionTimeText : 'all' }</span></h2>
-                    <div ng-repeat="version in type.versions | orderBy:'-'">
-                        <a
-                          ng-repeat="os in type.oses"
-                          href="#"
-                          download="{ !disabled ? os.filename + os.extension : undefined }"
-                          target="_blank"
-                          class="download-button{disabled ? " disabled" : ""}"
-                          ng-if="version == type.version || type.showAll"
-                          ng-init="disabled = os.betaVersions.indexOf(version) < 0"
-                          ng-disabled="disabled"
-                        >
-                            <button>
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <div class="icon-container"><img ng-src="/images/{os.key}.svg" /></div>
-                                        </td>
-                                        <td>
-                                            { os.name } <span ng-if="type.showAll">v{ version }</span> build
-                                        </td>
-                                    </tr>
-                                </table>
-                            </button>
-                        </a>
-                    </div>
-                </div>
+                {#if !currentOs}
+                <p>{ os } is not currently supported by Flat.</p>
+                {/if}
             </div>
             {#if currentOs}
             <div use:slide id="installation" class="slide">
@@ -50,8 +24,18 @@
                 <hr>
                 <h4>Pre-requisites</h4>
                 <ul>
-                    <li>Java 1.8 or later <span class="why-java gray" ng-init="whyJava = false" ng-click="whyJava = true"><span ng-if="!whyJava">why java?</span><span ng-if="whyJava">The first iteration of the compiler is written in Java.</span></span></li>
-                    <li>GCC compiler <span class="why-java gray" ng-init="whyGcc = false" ng-click="whyGcc = true"><span ng-if="!whyGcc">why gcc?</span><span ng-if="whyGcc">C is the most mature compilation target currently, and is the only language that comes pre-packaged with the installation.</span></span></li>
+                    <li>Java Runtime 1.8 or later
+                        <span class="why gray">
+                            {#if !whyJava}<span class="tooltip" on:click={() => whyJava = true}>why java?</span>{/if}
+                            {#if whyJava}<span>The first iteration of the compiler is written in Java</span>{/if}
+                        </span>
+                    </li>
+                    <li>Node 16 or later
+                        <span class="why gray">
+                            {#if !whyNode}<span class="tooltip" on:click={() => whyNode = true}>why node?</span>{/if}
+                            {#if whyNode}<span>Flat's package manager <a target="_blank" href="https://github.com/FlatLang/Airship">Airship</a> runs on node</span>{/if}
+                        </span>
+                    </li>
                 </ul>
                 <p>After the installer has finished, you are ready to <a href="/docs/getting-started/hello-world">write your first program</a>.</p>
             </div>
@@ -68,6 +52,9 @@
 
     import { jscd } from '/src/util';
     import { slide } from '/src/slide';
+
+    let whyJava = false;
+    let whyNode = false;
 
     let buildVersion;
     let disabled = false;
